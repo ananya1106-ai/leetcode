485b71a7479687d03e6abb659fb75a044ed8dbb8
Ananya Agarwal <aglananya11@gmail.com>
2026-08-25 18:12:17 +0530

    Time: 4 ms (65.71%) | Memory: 13.5 MB (52.81%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> v;
+        int i=0,j=n;
+        while(i<n){
+            v.push_back(nums[i]);
+            i++;
+             v.push_back(nums[j]);
+             j++;
+        }
+        return v;
+    }
+};
