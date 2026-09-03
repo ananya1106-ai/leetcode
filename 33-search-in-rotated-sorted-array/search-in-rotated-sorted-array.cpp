e691532c5738c9b4b86295a40d45d022ee80ea8b
Ananya Agarwal <aglananya11@gmail.com>
2026-09-03 22:27:48 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (37.04%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,47 +1,53 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int low= 0,high=n-1,res=-1;
+
+        int n= nums.size();
+        int low= 0,high=n-1;
         //min
         while(low <= high)
         {
             int guess= (low+high)/2;
+
+            if(nums[guess] == target)
+            return guess;
+
+            //PART 1
             if(nums[guess] > nums[n-1])
-            low= guess+1;
+            {
+                if(nums[guess] < target)
+                low= guess+1;
+                else
+                {
+                    if(nums[0] > target)
+                    {
+                        low= guess+1; //right
+                    }
+                    else
+                    {
+                        high= guess-1; //left
+                    }
+                }
+            }
+            
+            //PART 2
             else
             {
-                res= guess;
+                if(nums[guess] > target)
                 high= guess-1;
+                else
+                {
+                    if(nums[n-1] < target)
+                    {
+                         high= guess-1; //left
+                    }
+                    else
+                    {
+                        low= guess+1; //right
+                    }
+                }
             }
         }
-        // first half
-        int mn =  res;
-        low= 0 , high = mn-1;
-        while(low <= high)
-        {
-            int mid= (low+high)/2;
-            if(nums[mid] == target)
-            return mid;
-            else if(nums[mid] > target)
-            high = mid-1;
-            else
-            low= mid+1;
-        }
-
-        // second half
-        low= mn , high = n-1;
-        while(low <= high)
-        {
-            int mid= (low+high)/2;
-            if(nums[mid] == target)
-            return mid;
-            else if(nums[mid] > target)
-            high = mid-1;
-            else
-            low= mid+1;
-        }
-        return -1;
-
+    return -1;
     }
 };
