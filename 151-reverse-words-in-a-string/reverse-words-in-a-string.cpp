51a1bad99760301ba6131777c4f0da1282e56915
Ananya Agarwal <aglananya11@gmail.com>
2026-09-03 16:44:43 +0530

    Time: 3 ms (33.78%) | Memory: 11.3 MB (22.09%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<int> st;
+        int n= s.size();
+        string str="";
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[i] != ' ')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if(!st.empty())
+                {
+                while(!st.empty())
+                {
+                    char a= st.top();
+                    st.pop();
+                    str.push_back(a);
+                }
+                str.push_back(' ');
+            }
+            }
+        }
+            while(!st.empty())
+                {
+                    char a= st.top();
+                    st.pop();
+                    str.push_back(a);
+                }
+                int m= str.size();
+                if(str.size() >0 && str[m-1] == ' ')
+                {
+                    str.pop_back();
+                }
+                return str;
+    }
+};
