d9e47f9d7bde0f80c1a5ba61b9dcc5012ba642a6
Ananya Agarwal <aglananya11@gmail.com>
2026-09-05 12:43:28 +0530

    Time: 71 ms (21.04%) | Memory: 118.7 MB (11.48%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -4,27 +4,23 @@ public:
         
         vector<pair<int, double>> cars;
 
-        // position aur time store karo
         for (int i = 0; i < position.size(); i++) {
             double time = (double)(target - position[i]) / speed[i];
             cars.push_back({position[i], time});
         }
 
-        // position descending order
         sort(cars.rbegin(), cars.rend());
 
-        int fleets = 0;
-        double lastTime = 0;
+        stack<double> st;
 
-        for (auto car : cars) {
-            double time = car.second;
+        for (int i = 0; i < cars.size(); i++) {
+            double time = cars[i].second;
 
-            if (time > lastTime) {
-                fleets++;
-                lastTime = time;
+            if (st.empty() || time > st.top()) {
+                st.push(time);
             }
         }
 
-        return fleets;
+        return st.size();
     }
 };
