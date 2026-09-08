50866ceb500518acd696aa49e22cbb8f7479c4d5
Ananya Agarwal <aglananya11@gmail.com>
2026-09-08 23:30:12 +0530

    Time: 3 ms (45.18%) | Memory: 10 MB (11.39%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        unordered_map<char,int> mp1;
+        unordered_map<char,int> mp2;
+        if(m != n)
+        return false;
+        else
+        {
+            for(int i=0;i<n;i++)
+            {
+                mp1[s[i]]++;
+            }
+            for(int i=0;i<m;i++)
+            {
+                mp2[t[i]]++;
+            }
+        }
+        if(mp1 == mp2)
+        return true;
+        else
+        return false;
+        
+    }
+};
